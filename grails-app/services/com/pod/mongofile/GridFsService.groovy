912eb17ed8d2aa93b0b3b39706c4daa72aecc7b8
jtoddington <jtoddington@gmail.com>
2015-01-27 21:52:14 +0000

    Added tests and implementation.

--- grails-app/services/com/pod/mongofile/GridFsService.groovy
+++ grails-app/services/com/pod/mongofile/GridFsService.groovy
@@ -1,10 +1,14 @@
 package com.pod.mongofile
 
+import com.mongodb.BasicDBObject
+import com.mongodb.DB
+import com.mongodb.DBCursor
 import com.mongodb.DBObject
 import com.mongodb.Mongo
 import com.mongodb.gridfs.GridFS
+import com.mongodb.gridfs.GridFSInputFile
 import grails.transaction.Transactional
-import org.bson.BSONObject
+import org.bson.types.ObjectId
 import org.springframework.beans.factory.InitializingBean
 
 @Transactional
@@ -19,7 +23,7 @@ class GridFsService implements InitializingBean{
     void afterPropertiesSet(){
         this.mongoSettings = grailsApplication.config.mongodb
         mongo = new Mongo(mongoSettings.host.toString(), mongoSettings.port.intValue())
-        if (mongoSettings.bucket == null) {
+        if (mongoSettings.bucket == null || mongoSettings.bucket.size() == 0) {
             gridfs = new GridFS(mongo.getDB(mongoSettings.dbName.toString()))
         } else {
             gridfs = new GridFS(mongo.getDB(mongoSettings.dbName.toString()), mongoSettings.bucket.toString())
@@ -27,14 +31,14 @@ class GridFsService implements InitializingBean{
     }
 
     /**
-     *
+     * Save a file to the Mongo Grid FS store returns the OID of the object.
      * @param inputStream
      * @param contentType
      * @param filename
      * @return
      */
     def saveFile(inputStream, contentType, filename) {
-        def inputFile = gridfs.createFile(inputStream)
+        GridFSInputFile inputFile = gridfs.createFile(inputStream)
         log.info(inputFile)
         inputFile.setContentType(contentType)
         inputFile.setFilename(filename)
@@ -43,12 +47,47 @@ class GridFsService implements InitializingBean{
     }
 
     /**
-     *
+     * Retrieves a specific version of the file from 0-N
+     * @param filename String
+     * @param version Integer
+     */
+    def retrieveVersion(filename, version){
+        def cursor = retrieveFileList(filename)
+        def t = cursor.toArray()
+        def selected
+        t.eachWithIndex { dbFile, i ->
+            if(i==version-1){
+                selected = dbFile
+            }
+        }
+        return selected
+        //throw new Exception("Version not found.")
+    }
+
+    /**
+     * Retrieve the most recent version of a file.
      * @param filename
      * @return
      */
     def retrieveFile(filename){
-        return gridfs.findOne(filename)
+        def res = gridfs.find(new BasicDBObject( "filename" , filename )) //Cant sort on this.... unless we use a comparator
+        DB db = mongo.getDB(mongoSettings.dbName.toString())
+        def fsCol = db.getCollection("fs")
+        def collection = fsCol.getCollection("files")
+        DBCursor files
+        files = collection.find(new BasicDBObject("filename", filename))
+        files = files.sort(new BasicDBObject("uploadDate", -1))
+        DBObject result = files.first()
+        return result
+    }
+
+    /**
+     *
+     * @param gridFsId
+     * @return
+     */
+    def retriveFile(ObjectId gridFsId){
+        return gridfs.find(gridFsId)
     }
 
     /**
@@ -62,11 +101,34 @@ class GridFsService implements InitializingBean{
 
     /**
      *
+     * @param gridFsId
      * @return
      */
-    def getFilesList() {
-        def cursor = gridfs.getFileList()
-        def t = cursor.toArray()
+    def deleteFile(ObjectId gridFsId) {
+        return gridfs.remove(gridFsId)
+    }
+
+    /**
+     * GEt a list of files sorted by most oldest first.
+     * @return ArrayList of type File
+     */
+    def retrieveFileList() {
+        return retrieveFileList(null)
+    }
+
+    /**
+     * Get a list of all files and versions for a specific file or all if null passed.
+     * @param filename
+     * @return
+     */
+    def retrieveFileList(String filename) {
+        def cursor
+        if(filename != null) {
+            cursor = gridfs.getFileList(new BasicDBObject("filename", filename))
+        }else{
+            cursor = gridfs.getFileList()
+        }
+        def t = cursor.sort(new BasicDBObject("uploadDate", 1)).toArray()
         t.each {
             println it
         }
--- test/integration/mongofile/GridFsServiceSpec.groovy
+++ test/integration/mongofile/GridFsServiceSpec.groovy
@@ -12,49 +12,100 @@ import spock.lang.Specification
 class GridFsServiceSpec extends Specification {
 
     def gridFsService
+    def FILE_NAME = "SampleScannedDoc.pdf"
+    def FILE_BASE_PATH = "test/resources/"
 
     def setup() {
-
     }
 
     def cleanup() {
+        //Delete everything!
+        def fl = gridFsService.retrieveFileList()
+        fl.each {
+            gridFsService.deleteFile(it.id)
+        }
     }
 
 
     void "test getting the most recent file"(){
         when:
-            def f = new File("test/resources/SampleScannedDoc.pdf")
+            def f = new File("${FILE_BASE_PATH}${FILE_NAME}")
             def fis= new FileInputStream(f.getAbsolutePath())
         then:
-            def result = gridFsService.saveFile(fis,"application/pdf",f.getName())
-            result != null
-            println result
+            def originalFileId = gridFsService.saveFile(fis,"application/pdf",f.getName())
+            originalFileId != null
         when:
-            def gridFsDbFile = gridFsService.retrieveFile(f.name)
+            println "Original File Id: " + originalFileId
+            def newFileId = gridFsService.saveFile(fis,"application/pdf",f.getName())
         then:
-            def date = gridFsDbFile.get("uploadDate")
-            date != null
-            println date
+            newFileId != null
+        when:
+            def gridFsDbFile = gridFsService.retrieveFile(FILE_NAME)
+        then:
+            def files = gridFsDbFile.getAt("uploadDate")
+            files != null
+
+            //Ok check for the latest as the returned file.
+
+
+            gridFsDbFile.id == newFileId //It's getting the old one!
     }
 
     void "test listing files"() {
         when:
-            def result = gridFsService.getFilesList()
+            def f = new File("${FILE_BASE_PATH}${FILE_NAME}")
+            def fis= new FileInputStream(f.getAbsolutePath())
+            def id = gridFsService.saveFile(fis,"application/pdf",f.getName())
+            id = gridFsService.saveFile(fis,"application/pdf",f.getName())
+            id = gridFsService.saveFile(fis,"application/pdf",f.getName())
+            def result = gridFsService.retrieveFileList()
         then:
-            result.size() > 0
+            result.size() >= 3
     }
+
+    void "test getting a file by id"(){
+        when:
+            def f = new File("${FILE_BASE_PATH}${FILE_NAME}")
+            def fis= new FileInputStream(f.getAbsolutePath())
+            def id, selId = gridFsService.saveFile(fis,"application/pdf",f.getName())
+            id = gridFsService.saveFile(fis,"application/pdf",f.getName())
+            selId = gridFsService.saveFile(fis,"application/pdf",f.getName())
+        then:
+            def retrievedFile = gridFsService.retriveFile(selId)
+            retrievedFile.id == selId
+    }
+
+
+    void "test getting a particular version of  a file"(){
+        when:
+            def f = new File("${FILE_BASE_PATH}${FILE_NAME}")
+            def fis= new FileInputStream(f.getAbsolutePath())
+            def id, versionId
+            id = gridFsService.saveFile(fis,"application/pdf",f.getName()) //1
+            id = gridFsService.saveFile(fis,"application/pdf",f.getName()) //2
+            id = gridFsService.saveFile(fis,"application/pdf",f.getName()) //3
+            versionId = gridFsService.saveFile(fis,"application/pdf",f.getName()) //4
+            id = gridFsService.saveFile(fis,"application/pdf",f.getName()) //5
+            id = gridFsService.saveFile(fis,"application/pdf",f.getName()) //6
+        then:
+            def gridFsDbFile = gridFsService.retriveFile(versionId)
+            def versionGridFsDbFile = gridFsService.retrieveVersion(f.getName(), 4) //Fourth version!
+            versionId == versionGridFsDbFile.id
+            //gridFsDbFile.id == versionGridFsDbFile.id
+    }
+
 
     void "test adding a file"() {
         given:
-        def x = 10
-        def f = new File("test/resources/SampleScannedDoc.pdf")
-        def fis= new FileInputStream(f.getAbsolutePath())
-        def result = gridFsService.saveFile(fis,"application/pdf",f.getName())
+            def x = 10
+            def f = new File("${FILE_BASE_PATH}${FILE_NAME}")
+            def fis= new FileInputStream(f.getAbsolutePath())
+            def result = gridFsService.saveFile(fis,"application/pdf",f.getName())
 
         when:
-        x++
+            x++
 
         then:
-        x == 11
+            x == 11
     }
 }
